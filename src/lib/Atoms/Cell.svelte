f1bd533bed458beba66eee837c9bc4e7c593958e
musagenius345 <musabadru@gmail.com>
2023-11-29 01:25:57 +0300

    minor: cell design

--- src/lib/Atoms/Cell.svelte
+++ src/lib/Atoms/Cell.svelte
@@ -1,11 +1,13 @@
-<div class="cell">
+<div class="cell" >
 <slot></slot>
 </div>
   <style>
   .cell {
-  width: 2.5rem;
-  height: 2.5rem;
-  font-size: 1rem;
+    max-width: 100%;
+    --size: 2.25rem;
+  width: var(--size);
+  height: var(--size);
+  font-size: clamp(1.35rem, 1.2893rem + 0.3036vw, 1.5625rem);  
   background-color: var(--bg);
   cursor: pointer;
   font-family: 'Varela', 'Permanent Marker', serif;
@@ -23,7 +25,28 @@
   outline-offset: -5px;
   border-radius: 50%;
 }
+@media(min-width: 450px){
+    .cell{
+      --size: 3rem;
+    }
+  }
 
+@media(min-width: 575px){
+    .cell{
+      --size: 3.97rem;
+    }
+  }
 
+@media(min-width: 675px){
+    .cell{
+      --size: 4.57rem;
+    }
+  }
+
+@media(min-width: 950px){
+    .cell{
+      --size: 5.75rem;
+    }
+  }
 
 </style>
--- src/lib/Organisms/Game.svelte
+++ src/lib/Organisms/Game.svelte
@@ -14,8 +14,21 @@
   }
 </script>
 
-<div class="game breakout">
+<div class="game">
 {#each table as grid}
    <Cell>{removeDot(grid)}</Cell>
 {/each}
 </div>
+
+  <style>
+.game {
+  display: grid;
+  grid-template-columns: repeat(9,1fr);
+  grid-template-rows: repeat(9, 1fr);
+  gap: 0.18rem 0;
+  background-color: var(--ttt);
+  margin-block: 0.8rem;
+}
+
+
+</style>

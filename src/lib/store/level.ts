1680b2231f86964b61873df4946c3865f5c4f2b0
musagenius345 <musabadru@gmail.com>
2023-11-29 11:21:27 +0300

    better annotation

--- src/lib/store/level.ts
+++ src/lib/store/level.ts
@@ -1,4 +1,12 @@
-import { persisted } from "svelte-persisted-store"
 
- export const levels = ['easy', 'medium', 'hard', 'very-hard', 'insane', 'inhumane']
- export let selectedLevel = persisted('selectedLevel', 'easy')
+import { persisted } from "svelte-persisted-store";
+
+// Define the levels array
+export const levels = ['easy', 'medium', 'hard', 'very-hard', 'insane', 'inhumane'] as const;
+
+// Create a type for the union of level strings
+export type Level = typeof levels[number];
+
+// Use the level type in the persisted store
+export const selectedLevel = persisted<Level>('selectedLevel', 'easy');
+
